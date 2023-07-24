8964719a46368222da1a65ee21bc08ed3f07ea2f
sanmu <578595193@qq.com>
2023-07-24 12:05:54 +0800

    FIX: 兼容cargo source和cargo-source

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,31 @@ use cargo_source::*;
 use clap::{arg, Command};
 
 fn main() {
+    let mut args: Vec<String> = std::env::args().collect();
+    if args.len() >= 2 && args[1] != "source" {
+        args.insert(1, "source".to_string())
+    }
+
     let mut m = init_command();
-    match m.get_matches_mut().subcommand() {
-        Some(("source", arg_matches)) => match arg_matches.subcommand() {
-            Some(("list", _)) => ls(),
-            Some(("use", sub_m)) => {
-                if let Some(c) = sub_m.get_one::<String>("source") {
-                    switch(c);
+    match m.try_get_matches_from_mut(args) {
+        Ok(arg_matched) => match arg_matched.subcommand() {
+            Some(("source", arg_matches)) => match arg_matches.subcommand() {
+                Some(("list", _)) => ls(),
+                Some(("use", sub_m)) => {
+                    if let Some(c) = sub_m.get_one::<String>("source") {
+                        switch(c);
+                    }
                 }
-            }
-            Some(("add", sub_m)) => {
-                let registry_name = sub_m.get_one::<String>("name").unwrap();
-                let registry_url = sub_m.get_one::<String>("url").unwrap();
-                insert_registry(registry_name, registry_url);
-            }
+                Some(("add", sub_m)) => {
+                    let registry_name = sub_m.get_one::<String>("name").unwrap();
+                    let registry_url = sub_m.get_one::<String>("url").unwrap();
+                    insert_registry(registry_name, registry_url);
+                }
+                _ => m.print_help().unwrap(),
+            },
             _ => m.print_help().unwrap(),
         },
-        _ => ()
+        _ => m.print_help().unwrap(),
     }
 }
 
