43a5c7e9c8ad1bde7053bbb57fc925ad6e207cd5
sanmu <578595193@qq.com>
2023-07-25 08:46:53 +0800

    FEAT: remove subcommand source parse

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,25 @@ use clap::{arg, Command};
 
 fn main() {
     let mut args: Vec<String> = std::env::args().collect();
-    if args.len() >= 2 && args[1] != "source" {
-        args.insert(1, "source".to_string())
+    if args.len() >= 2 && args[1] == "source" {
+        args.remove(1);
     }
+    println!("{:?}", args);
 
     let mut m = init_command();
     match m.try_get_matches_from_mut(args) {
         Ok(arg_matched) => match arg_matched.subcommand() {
-            Some(("source", arg_matches)) => match arg_matches.subcommand() {
-                Some(("list", _)) => ls(),
-                Some(("use", sub_m)) => {
-                    if let Some(c) = sub_m.get_one::<String>("source") {
-                        switch(c);
-                    }
+            Some(("list", _)) => ls(),
+            Some(("use", sub_m)) => {
+                if let Some(c) = sub_m.get_one::<String>("source") {
+                    switch(c);
                 }
-                Some(("add", sub_m)) => {
-                    let registry_name = sub_m.get_one::<String>("name").unwrap();
-                    let registry_url = sub_m.get_one::<String>("url").unwrap();
-                    insert_registry(registry_name, registry_url);
-                }
-                _ => m.print_help().unwrap(),
-            },
+            }
+            Some(("add", sub_m)) => {
+                let registry_name = sub_m.get_one::<String>("name").unwrap();
+                let registry_url = sub_m.get_one::<String>("url").unwrap();
+                insert_registry(registry_name, registry_url);
+            }
             _ => m.print_help().unwrap(),
         },
         _ => m.print_help().unwrap(),
@@ -32,23 +30,21 @@ fn main() {
 }
 
 fn init_command() -> Command {
-    Command::new("cargo")
+    Command::new("")
         .about("crate 源切换工具")
         // .arg(
         //     Arg::new("in_file")
         // )
-        .subcommand(
-            Command::new("source").subcommands([
-                Command::new("list").about("列出当前可用源").alias("ls"),
-                Command::new("use")
-                    .about("使用指定源")
-                    .arg(arg!(<source> "前选择源名称").required(true)),
-                Command::new("add")
-                    .about("添加源")
-                    .arg(arg!(<name> "源名称").required(true))
-                    .arg(arg!(<url> "源地址").required(true)),
-            ]),
-        )
+        .subcommands([
+            Command::new("list").about("列出当前可用源").alias("ls"),
+            Command::new("use")
+                .about("使用指定源")
+                .arg(arg!(<source> "前选择源名称").required(true)),
+            Command::new("add")
+                .about("添加源")
+                .arg(arg!(<name> "源名称").required(true))
+                .arg(arg!(<url> "源地址").required(true)),
+        ])
         .after_help(
             "Longer ======= explanation to appear after the options when \
                 displaying the help information from --help or -h",
